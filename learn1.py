99ad7005ad9906b85a105a574dda5ee99b6df4bb
Natsuki Oshiro <e195767@ie.u-ryukyu.ac.jp>
2021-10-12 17:47:20 +0900

    add ex1.8 & ex1.9

--- learn1.py
+++ learn1.py
@@ -1,7 +1,25 @@
 import dataset1
-
+import numpy as np
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 #ex1.7
 
 path = "/Users/e195767/VS code/Data_Mining_Code/exam/"
 data = dataset1.load_tsv_from_df(path, "dataset1.tsv")
-print(data)
+
+
+#ex1.8 & ex1.9
+
+def learn(dataset, test_size, random_state=0):
+    feature = dataset["観測値"]
+    target = dataset["真値"]
+    X_train, y_train, X_test, y_test = train_test_split(feature, target, random_state=random_state, test_size=test_size)
+
+    X_train = np.array(X_train).reshape(1, -1)
+    y_train = np.array(y_train).reshape(1, -1)
+    X_test = np.array(X_test).reshape(1, -1)
+    y_test = np.array(y_test).reshape(1, -1)
+
+    lr = linear_model.LinearRegression()
+    lr.fit(X_train, y_train)
+    return lr, X_train, y_train, X_test, y_test
