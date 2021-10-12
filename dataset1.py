73da3b206a709bb4b8fe027de35220966144a7b6
Natsuki Oshiro <e195767@ie.u-ryukyu.ac.jp>
2021-10-12 15:20:58 +0900

    add ex1.2

--- dataset1.py
+++ dataset1.py
@@ -7,6 +7,10 @@ font_path = '/Library/Fonts/Arial Unicode.ttf'
 font_prop = font_manager.FontProperties(fname = font_path)
 matplotlib.rcParams['font.family'] = font_prop.get_name()
 
+path = "/Users/e195767/VS code/Data_Mining_Code/exam/"
+
+#ex1.1
+
 def true_function(x):
     '''
     >>> true_function(0)
@@ -27,5 +31,22 @@ plt.title("ture_function")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.legend()
-plt.savefig("ex1.1.png")
+#plt.savefig(path + "ex1.1.png")
+#plt.show()
+
+#ex1.2
+import random
+import pandas as pd
+random.seed(0)
+
+n = [round(random.uniform(-1, 1), 2) for _ in range(20)]
+true_val = [true_function(i) for i in n]
+
+data = np.array([n, true_val]).T
+
+df = pd.DataFrame(data, columns=["観測点", "真値"])
+
+for i in range(len(n)):
+    plt.plot(n[i], true_val[i], marker=".", markersize=10)
+plt.savefig(path + "ex1.2.png")
 plt.show()
